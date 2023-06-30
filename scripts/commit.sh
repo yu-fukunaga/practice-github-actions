466a42596c5ce209e7c60e7015eb4027786c6c47
yu-fukunaga <92198819+yu-fukunaga@users.noreply.github.com>
2023-06-30 18:59:56 +0900

    fix commit.sh

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -6,13 +6,14 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
+git checkout -q -b ${BRANCH}
+
 touch sample.txt
 
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 
-git checkout -q -b ${BRANCH}
 git status
 git add .
 git commit -m "Auto commit"
-git push origin ${BRANCH}
+git push https://${GITHUB_TOKEN}@github.com/yu-fukunaga/practice-github-actions.git ${BRANCH}
